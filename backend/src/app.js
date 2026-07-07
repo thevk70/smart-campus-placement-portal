f750ebdb1500cfc1fbd5916741779f7270c5bc17
Vishwajeet Kumar <100605183+thevk70@users.noreply.github.com>
2026-07-07 10:32:12 +0530

    Enable helmet middleware for security

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
-//app.use(helmet());
+app.use(helmet());
 
 app.use("/api",routes);
 app.use(errorHandler);
